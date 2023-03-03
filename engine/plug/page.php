0e5e2c1e0c45532ef1399fbef6286db5fb29c07a
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-03-03 15:49:29 +0700

    Update

--- engine/plug/page.php
+++ engine/plug/page.php
@@ -8,7 +8,7 @@ From::_('page', static function (?string $value, $eval = false) {
         // Add empty header
         $value = YAML\SOH . "\n" . YAML\EOT . "\n\n" . $value;
     }
-    $v = From::YAML($value, '  ', true, $eval);
+    $v = From::YAML($value, '  ', "\t", $eval);
     return $v[0] + ['content' => $v["\t"] ?? null];
 });
 
