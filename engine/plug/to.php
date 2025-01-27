77133a2605eb0e8d798a77abd22ac0af43824731
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-01-27 08:52:28 +0700

    Use the New `apart()` Function

--- engine/plug/to.php
+++ engine/plug/to.php
@@ -1,129 +1,84 @@
 <?php
 
 To::_('description', static function (?string $value, $max = 200): ?string {
-    if (!$value) {
-        return null;
-    }
-    // Add a space at the end of the block tag(s) that will be removed.
-    // So that `<p>asdf.</p><p>asdf</p>` becomes `asdf. asdf` and not `asdf.asdf`.
-    $value = preg_replace(['/\s+/', '/\s*(<\/(?:' . implode('|', [
-        'address',
-        'article',
-        'blockquote',
-        'd[dt]',
-        'details',
-        'div',
-        'figcaption',
-        'figure',
-        'footer',
-        'h[123456r]',
-        'header',
-        'li',
-        'main',
-        'nav',
-        'p',
-        'pre',
-        'section',
-        'summary',
-        't[dh]'
-    ]) . ')>)\s*/i'], [' ', '$1 '], $value);
-    $value = trim(strip_tags($value, [
-        'a',
-        'abbr',
-        'b',
-        'bdi',
-        'bdo',
-        'br',
-        'cite',
-        'code',
-        'data',
-        'del',
-        'dfn',
-        'em',
-        'i',
-        'ins',
-        'kbd',
-        'mark',
-        'q',
-        's',
-        'samp',
-        'small',
-        'span',
-        'strong',
-        'sub',
-        'sup',
-        'time',
-        'u',
-        'var',
-        'wbr'
-    ]));
-    if ("" === $value) {
+    if ("" === ($value ?? "")) {
         return null;
     }
     if (is_int($max)) {
         $max = [$max, '&#x2026;'];
     }
-    $utf8 = extension_loaded('mbstring');
-    // <https://stackoverflow.com/a/1193598/1163000>
-    if (false !== strpos($value, '<') || false !== strpos($value, '&')) {
-        $out = "";
-        $done = $i = 0;
-        $tags = [];
-        while ($done < $max[0] && preg_match('/<(?:\/[a-z\d:.-]+|[a-z\d:.-]+(?:\s(?:"[^"]*"|\'[^\']*\'|[^>])*)?)>|&(?:[a-z\d]+|#\d+|#x[a-f\d]+);|[\x80-\xFF][\x80-\xBF]*/i', $value, $m, PREG_OFFSET_CAPTURE, $i)) {
-            $tag = $m[0][0];
-            $pos = $m[0][1];
-            $str = substr($value, $i, $pos - $i);
-            if ($done + strlen($str) > $max[0]) {
-                $out .= substr($str, 0, $max[0] - $done);
-                $done = $max[0];
-                break;
+    $m = extension_loaded('mbstring');
+    if (false !== strpos($value, '&') || false !== strpos($value, '<')) {
+        $c = 0;
+        $keep = P . implode(P, ['a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'cite', 'code', 'data', 'del', 'dfn', 'em', 'i', 'ins', 'kbd', 'mark', 'q', 's', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'time', 'u', 'var', 'wbr']) . P;
+        $r = "";
+        $raw = ['audio', 'embed', 'noscript', 'script', 'style', 'textarea', 'video'];
+        // <https://html.spec.whatwg.org/multipage/syntax.html#void-elements>
+        $void = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr'];
+        $stack = [];
+        foreach (apart($value, $raw, $void) as $v) {
+            if (-1 === $v[1]) {
+                if ($c >= $max[0]) {
+                    break;
+                }
+                $c += 2;
+                $r .= $v[0] . ' ';
+                continue;
             }
-            $out .= $str;
-            $done += strlen($str);
-            if ($done >= $max[0]) {
-                break;
-            }
-            if ('&' === $tag[0] || ord($tag) >= 0x80) {
-                $out .= $tag;
-                ++$done;
-            } else {
-                // `tag`
-                $n = trim(strtok($tag, "\n\r\t "), '<>/');
-                // `</tag>`
-                if ('/' === $tag[1]) {
-                    $open = array_pop($tags);
-                    assert($open === $n); // Check that tag(s) are properly nested!
-                    $out .= $tag;
-                // `<tag/>` or <https://www.w3.org/TR/2011/WD-html-markup-20110113/syntax.html#void-element>
-                } else if ('/>' === substr($tag, -2) || preg_match('/^<(?:area|base|br|col|command|embed|hr|img|input|keygen|link|meta|param|source|track|wbr)(?=[\s>])/i', $tag)) {
-                    $out .= $tag;
-                // `<tag>`
-                } else {
-                    $out .= $tag;
-                    $tags[] = $n;
+            if (0 === $v[1]) {
+                $v[0] = trim(preg_replace('/\s+/', ' ', $v[0]));
+                $c += $m ? mb_strlen($v[0]) : strlen($v[0]);
+                if ("" !== $v[0]) {
+                    $c += 1;
+                    $r .= $v[0] . ' ';
+                    if ($c > $max[0]) {
+                        $r = $m ? mb_substr($r, 0, $max[0] - $c) : substr($r, 0, $max[0] - $c);
+                        break;
+                    }
+                    continue;
                 }
             }
-            // Continue after the tag…
-            $i = $pos + strlen($tag);
+            if (1 === $v[1]) {
+                $k = rtrim(strtok(substr($v[0], 1), " \n\r\t>"), '/');
+                if ('br' === $k || 'hr' === $k) {
+                    if ($c >= $max[0]) {
+                        break;
+                    }
+                    $c += 1;
+                    $r .= ' ';
+                    continue;
+                }
+                continue;
+            }
+            if (2 === $v[1]) {
+                $k = rtrim(strtok(substr($v[0], 1), " \n\r\t>"), '/');
+                if (false === strpos($keep, P . trim($k, '/') . P)) {
+                    continue;
+                }
+                if ('/' === $k[0]) {
+                    $r = trim($r) . $v[0] . ($c >= $max[0] ? "" : ' ');
+                    array_pop($stack);
+                    continue;
+                }
+                $r .= $v[0];
+                $stack[] = $k;
+                continue;
+            }
         }
-        // Print rest of the text…
-        if ($done < $max[0] && $i < strlen($value)) {
-            $out .= substr($value, $i, $max[0] - $done);
+        $r = trim($r);
+        while ($k = array_pop($stack)) {
+            $r .= '</' . $k . '>';
         }
-        // Close any open tag(s)…
-        while ($close = array_pop($tags)) {
-            $out .= '</' . $close . '>';
+        if ("" === trim(strip_tags($r))) {
+            return null;
         }
-        $out = trim(preg_replace('/\s*<br(?:\s(?:"[^"]*"|\'[^\']*\'|[^>])*)?>\s*/', ' ', $out));
-        $value = trim(strip_tags($value));
-        $count = $utf8 ? mb_strlen($value) : strlen($value);
-        $out = trim($out) . ($count > $max[0] ? $max[1] : "");
-        return "" !== $out ? $out : null;
+        return $r . ($c > $max[0] ? $max[1] : "");
     }
-    $out = $utf8 ? mb_substr($value, 0, $max[0]) : substr($value, 0, $max[0]);
-    $count = $utf8 ? mb_strlen($value) : strlen($value);
-    $out = trim($out) . ($count > $max[0] ? $max[1] : "");
-    return "" !== $out ? $out : null;
+    ;
+    if ("" === ($r = trim($m ? mb_substr($value, 0, $max[0]) : substr($value, 0, $max[0])))) {
+        return null;
+    }
+    return $r . (($m ? mb_strlen($r) : strlen($r)) > $max[0] ? $max[1] : "");
 });
 
 To::_('page', static function (?array $value): ?string {
